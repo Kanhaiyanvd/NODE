007d5a06281d3657b948707e0fdd1fe593b9e186
Kanhaiyanvd <118625121+Kanhaiyanvd@users.noreply.github.com>
2022-12-22 16:25:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 4000;
+// const hostname = '127.0.0.1';
+// const port = 4000;
 
-const server = http.createServer((req,res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.write('My name is Kanhaiya');
-    res.end();
-})
-server.listen(port, hostname , ()=>{
-    console.log(`server is running at http://${hostname}:${port}/`)
-});
+// const server = http.createServer((req,res) =>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','text/plain');
+//     res.write('My name is Kanhaiya');
+//     res.end();
+// })
+// server.listen(port, hostname , ()=>{
+//     console.log(`server is running at http://${hostname}:${port}/`)
+// });
